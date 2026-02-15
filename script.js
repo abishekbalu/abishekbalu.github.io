c8da4a89f45f6a9754d041a39aed405f2e2da48e
abishekbalu <abishekbalu@outlook.com>
2026-02-15 21:01:48 +0100

    Introduce responsive nav bar
    *) NEEDS IMPROVEMENT
    
    The click function works, but the display
    of menu items need further lookup

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function responsiveNavBar() {
+  var nav = document.getElementById("responsiveNavBar");
+  nav.classList.toggle("responsive");
+}
+/*
+const mobileNav = document.querySelector(".linedNav");
+const navbar = document.querySelector(".menubar");
+
+mobileNav.addEventListener("click", () => toggleNav());
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classsList.toggle("linedNav-active");
+};*/
