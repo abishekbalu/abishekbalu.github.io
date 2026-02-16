474d3419b3b4baefc3ae9a2a5384124f0206ac06
Abishek Balu Kothandaraman <abishek.balu.kothandaraman@ericsson.com>
2026-02-16 06:48:36 +0100

    Introduced left side responsive nav bar

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-
+/*
 function responsiveNavBar() {
   var nav = document.getElementById("responsiveNavBar");
   nav.classList.toggle("responsive");
-}
-/*
+}*/
+
 const mobileNav = document.querySelector(".linedNav");
 const navbar = document.querySelector(".menubar");
 
@@ -13,4 +13,4 @@ mobileNav.addEventListener("click", () => toggleNav());
 const toggleNav = () => {
   navbar.classList.toggle("active");
   mobileNav.classsList.toggle("linedNav-active");
-};*/
+};
